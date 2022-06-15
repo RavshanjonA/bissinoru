6b739b1089cbf6f35e501475970b2924e28c7c8a
Ahmadjonov-98 <ahmadjonovr.98@gmail.com>
2022-06-15 09:23:31 +0500

    create orderitem model

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,17 @@ class Item(models.Model):
     def __str__(self) -> str:
         return self.title
 
+class OrderItem(models.Model):
+    items = models.ManyToManyField("blog.Item", null=True, blank=True)
+    name = models.CharField(max_length=128, null=False, blank=False)
+    sum_cost = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.sum_cost}"
+    # def save(self, force_insert=False, force_update=False, using=None,
+    #          update_fields=None):
+    #     if not self.sum_cost:
+    #         self.sum_cost =
+    #
+
+
