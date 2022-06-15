53d233889d571330a12986213ab04718905a9ecc
Ahmadjonov-98 <ahmadjonovr.98@gmail.com>
2022-06-15 09:25:32 +0500

    oderitem serializer and add image addres path

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,8 +1,22 @@
 from cgi import FieldStorage
 from rest_framework import serializers
-from .models import Item
+from .models import Item, OrderItem
+
 
 class ItemSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField('get_image_url')
+
+
+    def get_image_url(self,obj):
+        return obj.image.url
+
     class Meta:
         model = Item
+        fields = ["type","image", "image_url", "title", "size", "cost", "desctiption"]
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderItem
         fields = "__all__"
