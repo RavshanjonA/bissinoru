b40d6e4e0b9e0aab0bd1edbf8cc6418addfbe562
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2022-06-18 03:24:55 +0500

    productapiview and orderapiview

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from product.views import ProductAPIView, OrderAPIView
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
@@ -24,7 +26,7 @@ urlpatterns = [
                   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
                   path('admin/', admin.site.urls),
-                  # path('api/v1/items/', ItemViewSet.as_view(), name="item-list"),
-                  # path('api/v1/order_items/', OrderItemAPIView.as_view(), name="order-list"),
+                  path('api/v1/productss/', ProductAPIView.as_view(), name="product-list"),
+                  path('api/v1/orders/', OrderAPIView.as_view(), name="order-list"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
                                                                                          document_root=settings.STATIC_ROOT)
--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from product.models import Product, Order
+from product.serializers import ProductSerializer
+
+
+class ProductAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(data=serializer.data)
+
+
+class OrderAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        orders = Order.objects.all()
+        serializer = ProductSerializer(orders, many=True)
+        return Response(data=serializer.data)
