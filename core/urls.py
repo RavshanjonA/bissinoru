2b0806263216e63d32977b6c94f5efbec84e31ed
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2022-06-18 04:29:18 +0500

    register models

--- core/urls.py
+++ core/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
                   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
                   path('admin/', admin.site.urls),
-                  path('api/v1/productss/', ProductAPIView.as_view(), name="product-list"),
+                  path('api/v1/products/', ProductAPIView.as_view(), name="product-list"),
                   path('api/v1/orders/', OrderAPIView.as_view(), name="order-list"),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL,
                                                                                          document_root=settings.STATIC_ROOT)
--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from product.models import Product, Order
+
+
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    list_display = ["title", "price", "size"]
+
+
+@admin.register(Order)
+class ProductAdmin(ModelAdmin):
+    list_display = ["username", "phone", "summa"]
