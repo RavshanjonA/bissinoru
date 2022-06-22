c69f70cbbf303381685a1658f04cb63615d17bb0
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2022-06-22 19:32:59 +0500

    change static and media urls

--- core/settings.py
+++ core/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -120,6 +120,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 #     os.path.join(BASE_DIR, "static")
 # ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/' # 'http://myhost:port/media/'
+MEDIA_URL = 'media' # 'http://myhost:port/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
